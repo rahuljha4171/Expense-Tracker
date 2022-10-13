8d891474d556dbaa965c3f062266481a81d4b802
Rahul Jha <104366355+rahuljha4171@users.noreply.github.com>
2022-10-13 23:57:22 +0530

    frontend complete

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,7 @@ import "./style.css";
 const Home = () => {
   return (
     <div className="App">
-      <div className="container max-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
           Expense Tracker
         </h1>
@@ -15,7 +15,7 @@ const Home = () => {
           {/* Chart */}
           <Graph />
           {/* Form */}
-          <Form/>
+          <Form />
         </div>
       </div>
     </div>
